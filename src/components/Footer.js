2ba0df26b69bb2f8f60c5d06bacd27b4a5cd93ae
Bianca-Fernandes <79364731+Bianca-Fernandes@users.noreply.github.com>
2023-01-26 16:02:35 +0530

    Add files via upload

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+const Footer = () => {
+  return (
+    <div className="footer-container">
+      <div className="footer">
+        <p>BritneyF10's Foodary © 2023</p>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ src/recipe.js
@@ -0,0 +1,46 @@
+export const my_recipe = {
+    recipe_1: {
+      name: 'Chirashi Don',
+      preparation_time: '30 minutes',
+      servings: '2',
+      img: '/img/recipe1.jpg',
+      ingredients: [
+        '1/2 cup rice',
+        '1/2 cup water',
+        '2 eggs',
+        '300g of salmon, tuna and salmon roe',
+      ],
+      instructions: [
+        'Cook rice and water in a rice cooker',
+        'Fry eggs in a pan',
+        'Serve rice, eggs and fish on a plate',
+      ],
+    },
+  
+    recipe_2: {
+      name: 'Biscoff Cheesecake',
+      preparation_time: '45 minutes',
+      servings: '6',
+      img: '/img/recipe2.png',
+      ingredients: [
+        '200g - Lotus Biscoff Spread (Cookie Butter)',
+        '1 tsp - Vanilla Extract',
+        '240g - Double Cream (Heavy/ Whipping) (1 cup)',
+        '10g - Gelatin',
+        '525g - Full Fat Cream Cheese (2 ⅓ cup)',
+        '225g - Lotus Biscoff Biscuits',
+        '65ml - Water',
+        '100g - White Chocolate',
+        '85g - Butter (melted)',
+        '70g - Powdered Sugar',
+      ],
+      instructions: [
+        'Crush the biscuits and mix butter & biscuit crumbs',
+        'Spread mixture into baking tin. Press firmly. Refrigerate',
+        'Whisk in the Biscoff spread & Vanilla Extract in the melted White Chocolate',
+        'Spoon mixture onto biscuit base',
+        'Pour melted Biscoff spread over the cheesecake. Spread out. Refrigerate to set',
+        'Pipe decorative border & top with a Biscoff biscuit. Refrigerate. Cut into neat slices',
+      ],
+    }
+}
